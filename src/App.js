025350c9f399727971eebed2dac1337d82ae4a15
albertassi88 <albertassi88@gmail.com>
2021-11-04 19:05:56 -0300

    instalaçao e criaçao do style-components na pagina Home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
-import Home from "./pages/Home";
+import Home from "./pages/Home/index";
+import { GlobalStyle } from "./style";
 
 function App() {
   return (
     <div >
+      <GlobalStyle />
       <Home />
     </div>
   );
---
+++ src/pages/Home/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Div } from "../Home/style";
+
+export default function Home() {
+    return (
+        <Div>
+            <p>Home</p>
+        </Div>
+    );
+}
